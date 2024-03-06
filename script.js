67c52ef78c990d1dc62d6cf0923e2164efb6980a
DaizuBuilding <asazumaknt0921@gmail.com>
2024-03-06 21:39:40 +0900

    fix

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function hamburger() {
-    document.querySelector('.hamburger').addEventListener('click', function(){
-        this.classList.toggle('active');
-        document.querySelector('.menu').classList.toggle('appear');
-    });
-}
 fetch("/Shinsho/header.html")
   .then((response) => response.text())
   .then((data) => document.querySelector(".header").innerHTML = data);
@@ -12,3 +6,10 @@ fetch("/Shinsho/footer.html")
   .then((response) => response.text())
   .then((data) => document.querySelector(".footer").innerHTML = data)
   .then(hamburger());
+
+  function hamburger() {
+    document.querySelector('.hamburger').addEventListener('click', function(){
+        this.classList.toggle('active');
+        document.querySelector('.menu').classList.toggle('appear');
+    });
+}
