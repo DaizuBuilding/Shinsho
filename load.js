8104fda60c3a7feae09cde35e976dd8363e5f33d
DaizuBuilding <asazumaknt0921@gmail.com>
2024-03-06 21:29:50 +0900

    upload

---
+++ load.js
@@ -0,0 +1,7 @@
+fetch("/Shinsho/header.html")
+  .then((response) => response.text())
+  .then((data) => document.querySelector(".header").innerHTML = data);
+
+fetch("/Shinsho/footer.html")
+  .then((response) => response.text())
+  .then((data) => document.querySelector(".footer").innerHTML = data);
--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-fetch("/Shinsho/header.html")
-  .then((response) => response.text())
-  .then((data) => document.querySelector(".header").innerHTML = data);
-
-fetch("/Shinsho/footer.html")
-  .then((response) => response.text())
-  .then((data) => document.querySelector(".footer").innerHTML = data);
+document.querySelector('.hamburger').addEventListener('click', function(){
+    this.classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('appear');
+});
