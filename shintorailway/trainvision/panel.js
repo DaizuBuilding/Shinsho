fe934cef710916c5ff821d646123482859abd950
DaizuBuilding <162403102+DaizuBuilding@users.noreply.github.com>
2026-04-25 01:37:34 +0900

    update

---
+++ shintorailway/trainvision/panel.js
@@ -0,0 +1,28 @@
+let digit = 0;
+document.getElementById('update').addEventListener('click', () => {
+    digit = 0;
+    returnWindow();
+});
+document.getElementById('departure').addEventListener('click', () => {
+    digit = 1;
+    returnWindow();
+});
+document.getElementById('arrive').addEventListener('click', () => {
+    window.opener.arriving();
+});
+document.getElementById('stopping').addEventListener('click', () => {
+    window.opener.stopping();
+})
+
+console.log(document.getElementById('selectsta').value);
+
+function returnWindow() {
+    const error = document.getElementById('error');
+    if (!window.opener || window.opener.closed) {
+        error.innerHTML = 'ビジョン画面が開かれていません。次のサイトにアクセスしてください：https://daizubuilding.github.io/Shinsho/shintorailway/trainvision/';
+        return false;
+    } else {
+        error.innerHTML = '';
+    }
+    window.opener.changeData(Number(document.getElementById('selectsta').value) - 1, document.getElementById('selectline').value, document.getElementById('selecttype').value, document.getElementById('selectcar').value);
+}
---
+++ shintorailway/trainvision/script2.js
@@ -0,0 +1,173 @@
+document.getElementById('loading').addEventListener('click', () => {
+    window.open('./panel.html', 'トレインビジョン操作パネル', 'width = 600, height = 400, scrollbars = 0');
+});
+
+let stationIndex; // 駅番号インデックス（0 スタート）
+let destinationNumber; // 行き先番号
+let lineNumber; // 路線番号
+let lineIndex; // 路線インデックス（Json 参照用）
+let typeNumber; // 種別番号
+let lineName = ""; // 路線名
+let lineNumbering = ""; // 路線ナンバリング
+let carNo; // 号車
+let stopsLength; // 停車駅数
+let terminalDigit = 0; // 主要駅判定
+let terminalDigitBack = 0; // 1 つ前の主要駅判定
+let informations; // informations.json
+
+let announceScript; // アナウンス原稿
+let announceIndex; // アナウンスインデックス
+
+// Json 読み込み
+async function getJson(stanum, linenum) {
+    const response = await fetch('./informations.json');
+    const data = await response.json();
+    informations = await data;
+
+    lineName = `${data.typesinfo[lineIndex].line}_${data.typesinfo[lineIndex].name}`; // 路線名
+    lineNumbering = data.typesinfo[lineIndex].numbering; // 路線ナンバリング
+    stopsLength = data.typesinfo[lineIndex].stops.length; // 停車駅数
+    terminalDigit = data.stationsinfo[stanum].terminal; // 主要駅判定
+    terminalDigitBack = data.stationsinfo[adjustStationIndex(stanum) + 1].terminal; // 1 つ前の主要駅判定
+}
+
+// パネルからのデータ取得と変更
+async function changeData(stanum, linenum, typenum, destnum, carnum) {
+    await getJson(); // Json データ取得
+    stationIndex = stanum;
+    lineIndex = setLineIndex(linenum, typenum, destnum);
+    typeNumber = typenum;
+    destinationNumber = setDestinationNumber(stanum, linenum, typenum, destnum);
+    carNo = carnum;
+    visionChange();
+}
+
+function setLineIndex(linenum, typenum, destnum) {
+    let num = '00';
+    if (linenum == 0) { num = toString(linenum) + toString(typenum); }
+    if (linenum > 0) { num = toString(linenum) + toString(destnum); }
+    return num;
+}
+
+// 停車駅インデックスの調整
+function adjustStationIndex(num) {
+    return num % stopsLength;
+}
+
+// 行き先番号
+function setDestinationNumber(stanum, linenum, typenum, destnum) {
+    let num;
+    if (linenum == 1) {
+        if (typenum == 0) {
+            if (stanum >= 25) { num = 9; }
+            else if (stanum >= 22) { num = 8; }
+            else if (stanum >= 20) { num = 7; }
+            else if (stanum >= 17) { num = 6; }
+            else if (stanum >= 15) { num = 5; }
+            else if (stanum >= 12) { num = 4; }
+            else if (stanum >= 8) { num = 3; }
+            else if (stanum >= 5) { num = 2; }
+            else if (stanum >= 3) { num = 1; }
+            else if (stanum >= 1) { num = 0; }
+        } else if (typenum == 1) {
+            if (stanum == 1 || stanum >= 26) { num = 0; }
+            else if (stanum >= 23) { num = 9; }
+            else if (stanum >= 21) { num = 8; }
+            else if (stanum >= 18) { num = 7; }
+            else if (stanum >= 16) { num = 6; }
+            else if (stanum >= 13) { num = 5; }
+            else if (stanum >= 9) { num = 4; }
+            else if (stanum >= 6) { num = 3; }
+            else if (stanum >= 4) { num = 2; }
+            else if (stanum >= 2) { num = 1; }
+        }
+    } else { num = destnum; }
+
+    return num;
+}
+
+// ビジョン情報変更
+const border = document.getElementById('border');
+const headerLines = document.querySelectorAll('.line');
+const headerNumberings = document.querySelectorAll('.headernumbering');
+const headerStaNames = document.querySelectorAll('.headerstaname');
+const destinations = document.querySelectorAll('.destination');
+const carNos = document.getElementById('carno');
+const stationNames1 = document.querySelectorAll('.ssname1');
+const stationNames2 = document.querySelectorAll('.ssname2');
+const stationNames3 = document.querySelectorAll('.ssname3');
+const stationNames4 = document.querySelectorAll('.ssname4');
+const nexts = document.querySelectorAll('.next');
+function visionChange() {
+    border.src = `./img/Linecolor_${lineNumbering}.png`;
+    headerLines.forEach(img => { // 路線名
+        const base = img.dataset.base;
+        img.src = `${base}${lineName}.png`;
+    });
+    headerNumberings.forEach(img => { // ナンバリング
+        const base = img.dataset.base;
+        img.src = `${base}${lineNumbering}_${stationIndex + 1}.png`;
+    });
+    headerStaNames.forEach(img => { // ヘッダー駅名表示
+        const base = img.dataset.base;
+        img.src = `${base}${stationIndex + 1}.png`;
+    });
+    destinations.forEach(img => { // 行先
+        const base = img.dataset.base;
+        img.src = `${base}${lineNumber}${typeNumber}${destinationNumber}.png`;
+    });
+    carNos.src = `${carNos.dataset.base}${carNo}.png`; // 号車表示
+    stationNames1.forEach(img => { 
+        const base = img.dataset.base;
+        img.src = `${base}${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)]}.png`;
+    });
+    stationNames2.forEach(img => {
+        const base = img.dataset.base;
+        img.src = `${base}${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex + 1)]}.png`;
+    });
+    stationNames3.forEach(img => {
+        const base = img.dataset.base;
+        img.src = `${base}${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex + 2)]}.png`;
+    });
+    stationNames4.forEach(img => {
+        const base = img.dataset.base;
+        img.src = `${base}${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex + 3)]}.png`;
+    });
+    nexts.forEach(img => { // 次は
+        const base = img.dataset.base;
+        img.src = `${base}next.png`;
+    });
+    document.getElementById('loading').classList.add('hidden'); // 起動画面の切り替え
+}
+
+// 到着
+function arriving() {
+    nexts.forEach(img => {
+        const base = img.dataset.base;
+        img.src = `${base}soon.png`;
+    });
+}
+
+// 停車中
+function stopping() {
+    nexts.forEach(img => {
+        const base = img.dataset.base;
+        img.src = `${base}now.png`;
+    });
+}
+
+// アナウンス設定
+function setAnnouncement() {
+    const announce;
+}
+
+// 言語切り替え
+const divisions = document.querySelectorAll('.division');
+let currentIndex = 0;
+function switchClass() {
+    divisions.forEach(div => div.classList.remove('displayed')); // 全てのクラスを一旦削除
+    divisions[currentIndex].classList.add('displayed'); // 現在の要素にだけクラス付与
+    currentIndex = (currentIndex + 1) % divisions.length; // 次のインデックスへ（ループさせる）
+}
+switchClass(); // 初回実行
+setInterval(switchClass, 3000); // 3 秒ごとに繰り返す
