d8ae54b7b604aa20b85fc29f763aaa0a99d8c862
DaizuBuilding <162403102+DaizuBuilding@users.noreply.github.com>
2026-04-25 02:02:37 +0900

    Update script2.js

--- shintorailway/trainvision/script2.js
+++ shintorailway/trainvision/script2.js
@@ -26,6 +26,7 @@ async function getJson(stanum, linenum) {
 
     console.log(data);
     console.log(data.typesinfo);
+    console.log(lineIndex);
     console.log(data.typesinfo[lineIndex]);
     await console.log(data.typesinfo[lineIndex]);
     lineName = `${data.typesinfo[toString(lineIndex)].line}_${data.typesinfo[lineIndex].name}`; // 路線名
