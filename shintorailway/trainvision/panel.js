9aa5cca3a729b8942dc71a95563a3dc4249633de
DaizuBuilding <162403102+DaizuBuilding@users.noreply.github.com>
2026-04-27 16:07:27 +0900

    update

--- shintorailway/trainvision/panel.js
+++ shintorailway/trainvision/panel.js
@@ -16,6 +16,45 @@ document.getElementById('stopping').addEventListener('click', () => {
     window.opener.stopping();
 })
 
+let selectline = document.getElementById('selectline');
+let selecttype = document.getElementById('selecttype');
+
+let informations;
+let linesInfos;
+async function getInformations() {
+    const response = await fetch('./informations.json');
+    const data = await response.json();
+    informations = data;
+
+    linesInfos = data.linesinfo;
+
+    updateOptions();
+}
+
+selecttype.innerHTML = '';
+
+function updateOptions() {
+    const lineinfo = linesInfos[selectline.value].types;
+    let i = 0;
+    lineinfo.forEach(type => {
+        const option = document.createElement('option');
+        option.value = i;
+        option.text = type;
+        i++;
+        selecttype.add(option);
+    });
+    // if (document.getElementById('selectline').value == 0) {
+        // const lineinfo = linesInfos[0].types;
+        // let i = 0;
+        // lineinfo.forEach(type => {
+            // const option = document.createElement('option');
+            // option.value = i;
+            // option.text = type;
+            // i++;
+            //  document.getElementById('selecttype').add(option);
+        // });
+}
+
 console.log(document.getElementById('selectsta').value);
 
 function returnWindow() {
