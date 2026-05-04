4071075fb20c819220bcd9aa429579ed8b55fb35
DaizuBuilding <162403102+DaizuBuilding@users.noreply.github.com>
2026-05-05 02:11:46 +0900

    Update script2.js

--- shintorailway/trainvision/script2.js
+++ shintorailway/trainvision/script2.js
@@ -113,11 +113,11 @@ function visionChange() {
     });
     headerNumberings.forEach(img => { // ナンバリング
         const base = img.dataset.base;
-        img.src = `${base}${lineNumbering}_${stationIndex + 1}.png`;
+        img.src = `${base}${lineNumbering}_${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)]}.png`;
     });
     headerStaNames.forEach(img => { // ヘッダー駅名表示
         const base = img.dataset.base;
-        img.src = `${base}${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)] + 1}.png`;
+        img.src = `${base}${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)]}.png`;
     });
     destinations.forEach(img => { // 行先
         const base = img.dataset.base;
@@ -169,7 +169,7 @@ let aLineDestination = '';
 function setAnnouncementScript() {
     if (lineNumber == 0) { aLineName = lineName } else { aLineName = lineNumbering; }
     aLineDestination = `destination/${lineNumber}${typeNumber}${destinationNumber}`;
-    let announceScript = [...(terminalDigitBack ? ['./announcement/00100.wav', './announcement/00201.wav', `./announcement/lines/Shinto_${aLineName}.wav`, `./announcement/${aLineDestination}.mp3`] : []), './announcement/00600.wav', `./announcement/stations/${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)] + 1}.wav`, `./announcement/stations/${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)] + 1}.wav`, './announcement/00702.wav'];
+    let announceScript = [...(terminalDigitBack ? ['./announcement/00100.wav', './announcement/00201.wav', `./announcement/lines/Shinto_${aLineName}.wav`, `./announcement/${aLineDestination}.mp3`] : []), './announcement/00600.wav', `./announcement/stations/${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)]}.wav`, `./announcement/stations/${informations.typesinfo[lineIndex].stops[adjustStationIndex(stationIndex)]}.wav`, './announcement/00702.wav'];
     announce(announceScript);
 }
 
